5be349760ebd211a65efe2c8811fbfb8d491b153
syoch <hacksyoch@gmail.com>
2021-09-10 21:51:35 +0900

    update: WIP

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,7 @@ use serenity::{
     async_trait,
     model::{
         gateway::Ready,
-        id::GuildId,
-        interactions::{
-            application_command::{
-                ApplicationCommand, ApplicationCommandInteractionDataOptionValue,
-                ApplicationCommandOptionType,
-            },
-            Interaction, InteractionResponseType,
-        },
+        interactions::{application_command::ApplicationCommand, Interaction},
     },
     prelude::*,
 };
@@ -22,37 +15,19 @@ struct Handler;
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
-            let content = match command.data.name.as_str() {
-                "ping" => "Hey, I'm alive!".to_string(),
-                "id" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected user option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected user object");
-
-                    if let ApplicationCommandInteractionDataOptionValue::User(user, _member) =
-                        options
-                    {
-                        format!("{}'s id is {}", user.tag(), user.id)
-                    } else {
-                        "Please provide a valid user".to_string()
-                    }
-                }
-                _ => "not implemented :(".to_string(),
-            };
-
-            if let Err(why) = command
+            let ret = command
                 .create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(content))
+                    response.interaction_response_data(|data| {
+                        data.components(|components| {
+                            components.create_action_row(|row| {
+                                row.create_button(|button| button.label("hello"))
+                            })
+                        })
+                    })
                 })
-                .await
-            {
+                .await;
+
+            if let Err(why) = ret {
                 println!("Cannot respond to slash command: {}", why);
             }
         }
@@ -61,75 +36,14 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
-        let commands = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
-            commands
-                .create_application_command(|command| {
-                    command.name("ping").description("A ping command")
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("id")
-                        .description("Get a user id")
-                        .create_option(|option| {
-                            option
-                                .name("id")
-                                .description("The user to lookup")
-                                .kind(ApplicationCommandOptionType::User)
-                                .required(true)
-                        })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("welcome")
-                        .description("Welcome a user")
-                        .create_option(|option| {
-                            option
-                                .name("user")
-                                .description("The user to welcome")
-                                .kind(ApplicationCommandOptionType::User)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("message")
-                                .description("The message to send")
-                                .kind(ApplicationCommandOptionType::String)
-                                .required(true)
-                                .add_string_choice(
-                                    "Welcome to our cool server! Ask me if you need help",
-                                    "pizza",
-                                )
-                                .add_string_choice("Hey, do you want a coffee?", "coffee")
-                                .add_string_choice(
-                                    "Welcome to the club, you're now a good person. Well, I hope.",
-                                    "club",
-                                )
-                                .add_string_choice(
-                                    "I hope that you brought a controller to play together!",
-                                    "game",
-                                )
-                        })
-                })
+        let res = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
+            commands.create_application_command(|command| {
+                command.name("ping").description("A ping command")
+            })
         })
         .await;
 
-        println!(
-            "I now have the following global slash commands: {:#?}",
-            commands
-        );
-
-        let guild_command = GuildId(123456789)
-            .create_application_command(&ctx.http, |command| {
-                command
-                    .name("wonderful_command")
-                    .description("An amazing command")
-            })
-            .await;
-
-        println!(
-            "I created the following guild command: {:#?}",
-            guild_command
-        );
+        println!("Created Commands: {:#?}", res);
     }
 }
 
@@ -144,6 +58,8 @@ async fn main() {
         .parse()
         .expect("application id is not a valid id");
 
+    println!("User ID {}", application_id);
+
     // Build our client.
     let mut client = Client::builder(token)
         .event_handler(Handler)
@@ -151,10 +67,6 @@ async fn main() {
         .await
         .expect("Error creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
