c6db531327f40e88729006b31d4b1cf99394406b
syoch <hacksyoch@gmail.com>
2021-09-11 09:11:42 +0900

    update: Tinyize Source

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,20 @@
 use std::env;
 
-use serenity::{
-    async_trait,
-    model::{
-        gateway::Ready,
-        interactions::{application_command::ApplicationCommand, Interaction},
-    },
-    prelude::*,
-};
+use serenity::{async_trait, model::gateway::Ready, prelude::*};
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            let ret = command
-                .create_interaction_response(&ctx.http, |response| {
-                    response.interaction_response_data(|data| {
-                        data.components(|components| {
-                            components.create_action_row(|row| {
-                                row.create_button(|button| button.label("hello"))
-                            })
-                        })
-                    })
-                })
-                .await;
-
-            if let Err(why) = ret {
-                println!("Cannot respond to slash command: {}", why);
-            }
-        }
-    }
-
-    async fn ready(&self, ctx: Context, ready: Ready) {
+    async fn ready(&self, _ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
-
-        let res = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
-            commands.create_application_command(|command| {
-                command.name("ping").description("A ping command")
-            })
-        })
-        .await;
-
-        println!("Created Commands: {:#?}", res);
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    // The Application Id is usually the Bot User Id.
     let application_id: u64 = env::var("APPLICATION_ID")
         .expect("Expected an application id in the environment")
         .parse()
@@ -60,7 +22,6 @@ async fn main() {
 
     println!("User ID {}", application_id);
 
-    // Build our client.
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .application_id(application_id)
