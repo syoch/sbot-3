9be60419e5ce7c01703b4ee26a2033073c5f5a2f
syoch <hacksyoch@gmail.com>
2021-11-12 09:02:10 -0500

    update: wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,69 @@
 use dotenv::dotenv;
-use serenity::{async_trait, model::gateway::Ready, prelude::*};
+use serenity::{
+    async_trait,
+    model::{
+        interactions::{
+            application_command::{ApplicationCommandInteraction, ApplicationCommandOptionType},
+            Interaction,
+        },
+        prelude::Ready,
+    },
+    prelude::*,
+};
 use std::env;
+
+fn get_option<T>(command: ApplicationCommandInteraction, i: usize) -> Option<Str> {
+    let option = command.data.options.get(i);
+    if let None = option {
+        return None;
+    }
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            match command.data.options.get(0)?.value.as_ref()?.as_str()? {
+                Some(s) => s,
+                _ => "Please specify message (this may be bug)".to_string(),
+            };
+
+            let ret = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response.interaction_response_data(|data| data.content(content))
+                })
+                .await;
+
+            if let Err(why) = ret {
+                println!("Error sending response: {:?}", why);
+            }
+        }
+    }
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        for guild in ready.guilds {
+            let name = guild.id().to_partial_guild(&ctx.http).await.unwrap().name;
+            match guild
+                .id()
+                .create_application_command(&ctx.http, |command| {
+                    command
+                        .name("say")
+                        .description("say specified message")
+                        .create_option(|option| {
+                            option
+                                .kind(ApplicationCommandOptionType::String)
+                                .name("message")
+                                .required(true)
+                                .description("message to say")
+                        })
+                })
+                .await
+            {
+                Ok(command) => println!("Successfully set guild command in {}", name),
+                Err(why) => println!("Error setting guild command in {}: {:?}", name, why),
+            }
+        }
     }
 }
 
